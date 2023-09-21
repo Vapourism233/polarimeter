69eec1f3f01c8a0106fa3948212f90c992fadadf
Vapourism233 <127520189+Vapourism233@users.noreply.github.com>
2023-09-21 21:40:17 +0900

    Update process.py

--- process.py
+++ process.py
@@ -2,15 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import spec
 
-data = np.load('fluct_1us.npy')
-t = np.arange(len(data)) * 1.0e-6
-n = 4096
-w = np.hanning(n)
-w = spec.normalize_area(w)
-(yt, f, t_spec) = spec.spectrogram(data, w)
-ts = t[1] - t[0] 
-f /= ts
-t_spec *= ts
+
 # plt.figure(figsize=(10, 6))
 # plt.plot(data)
 # plt.title('data points')
@@ -18,13 +10,6 @@ t_spec *= ts
 # plt.ylabel('Signal Amplitude')
 # plt.grid(True)
 # plt.show()
-
-from scipy.signal import welch
-# Parameters
-fs = 1e6 # sampling frequency in Hz
-window_size = 4096 # initial window size
-# Compute power spectral density (PSD) using the Welch method
-frequencies, psd = welch(data, fs=fs, nperseg=window_size)
 # # Plot the PSD
 # plt.figure(figsize=(10, 6))
 # plt.semilogy(frequencies, psd)
