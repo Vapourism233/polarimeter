875612f22d5a5ae4d82a50e6e9bbcbde3baceb6a
Vapourism233 <127520189+Vapourism233@users.noreply.github.com>
2023-09-21 21:39:56 +0900

    Update process.py

--- process.py
+++ process.py
@@ -39,27 +39,3 @@ frequencies, psd = welch(data, fs=fs, nperseg=window_size)
 # # peak_frequency = frequencies[peak]
 # # print(peak_frequency)
 
-n2 = 4096
-w2 = spec.flattop(n2)
-w2 = spec.normalize_height(w2)
-(yt2, f2, t_spec2) = spec.spectrogram(data, w2)
-f2 /= ts
-t_spec2 *= ts
-fcut = 239990.234375 # [Hz]
-i = spec.find_index(f2, fcut)
-a = np.abs(yt2[:, i]) * 2.0
-# plt.figure(3)
-# plt.plot(t_spec2, a)
-# plt.ylim((0, plt.ylim()[1]*1.5))
-# plt.xlabel('time [s]')
-# plt.ylabel('amplitude [V]')
-# plt.title('frequency = {:.3g} kHz'.format(fcut*1e-3))
-# plt.show()
-
-threshold = 0.6 * np.max(np.abs(a))
-# Find where the signal exceeds the threshold
-over_threshold = np.where(np.abs(a) > threshold)[0]
-# Compute the duration based on the start and end of the signal
-duration = (over_threshold[-1] - over_threshold[0]) / fs
-
-print(duration)q
